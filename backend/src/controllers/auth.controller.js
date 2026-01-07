315a7581664f44e128698276de115a755a6bb833
Karthik Reddy <karthikreddyy386@gmail.com>
2026-01-07 10:16:12 -0700

    signup section code refactoring

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -37,8 +37,8 @@ export const signup = async (req, res) => {
     });
 
     if (newUser) {
-      generateToken(newUser._id, res);
-      await newUser.save();
+      const savedUser = await newUser.save();
+      generateToken(savedUser._id, res);
       res.status(201).json({
         _id: newUser._id,
         fullname: newUser.fullname,
