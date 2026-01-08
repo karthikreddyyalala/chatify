618f66eb2fefb45aa9bd57d6b7b774a594eddab9
Karthik Reddy <karthikreddyy386@gmail.com>
2026-01-08 13:22:45 -0700

    update profile pic

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -3,6 +3,7 @@ import bcrypt from "bcryptjs";
 import { generateToken } from "../lib/utils.js";
 import { sendWelcomeEmail } from "../emails/emailHandlers.js";
 import {ENV} from "../lib/env.js";
+import cloudinary from "../lib/cloudinary.js";
 
 export const signup = async (req, res) => {
   const { fullname, email, password } = req.body;
@@ -87,3 +88,26 @@ export const logout = async (req, res) => {
   res.status(200).json({ message: "Logged out successfully" });
 
 };
+
+export const updateProfile = async (req, res) => {
+  try{
+    const { profilePic} = req.body;
+    if(!profilePic){
+      return res.status(400).json({ message: "Profile picture is required" });
+    }
+    const user = req.user._id;
+    const uploadResponse = await cloudinary.uploader.upload(profilePic);
+
+    const updatedUser = await User.findByIdAndUpdate(
+      user,
+      { profilepic: uploadResponse.secure_url },
+      { new: true }
+    );
+
+    res.status(200).json(updateUser);
+    
+   }catch (error){
+    console.error("Update profile error:", error);
+    return res.status(500).json({ message: "Server error during profile update" });  
+  }
+}
